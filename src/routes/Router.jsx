187946b62169ebdc5d9f73e4cbd04ae18b761f7c
Paula Jardim <paulajardimf@gmail.com>
2022-12-13 18:47:26 -0300

    loginPage finalizada

---
+++ src/routes/Router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateRecipePage from "../pages/CreateRecipePage/CreateRecipePage";
+import DetailsPage from "../pages/DetailsPage/DetailsPage";
+import HomePage from "../pages/HomePage/HomePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import SignupPage from "../pages/SignupPage/SignupPage";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recipe/:recipeId" element={<DetailsPage />} />
+        <Route path="/recipe/new" element={<CreateRecipePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
